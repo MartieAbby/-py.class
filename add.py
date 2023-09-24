ca57a56dd352dbd5c4431248bdadf0f493f70a29
MartieAbby <141751072+MartieAbby@users.noreply.github.com>
2023-09-24 20:08:43 +0000

    work

---
+++ add.py
@@ -0,0 +1,4 @@
+age=int(input("enter age\n"))
+current_year=int(input("enter current year\n"))
+dob=current_year-age
+print(dob)
--- bisola.py
+++ bisola.py
@@ -1,10 +1,2 @@
-a=1+2j
-print(a)
-print(type(a))
-b=5>4
-print(b)
-print(type(b))
-
 name = input("enter name: \n")
-name= name.replace("b","n")
-print("hello", name.upper())
+dob=input("enter dob")
